c8f1fdcbc110705e7b6f29458f1b810dabc83acc
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2025-01-01 10:36:41 +0200

    - add app routes

---
+++ lib/core/routing/routes.dart
@@ -0,0 +1,30 @@
+class Routes{
+  static const String login = '/login';
+  static const String signUp = '/signUp';
+  static const String forgotPassword = '/forgotPassword';
+  static const String home = '/home'; 
+  static const String profile = '/profile';
+  static const String settings = '/settings';
+  static const String editProfile = '/editProfile';
+  static const String changePassword = '/changePassword';
+  static const String changeLanguage = '/changeLanguage';
+  static const String changeTheme = '/changeTheme';
+  static const String category = '/category';
+  static const String product = '/product';
+  static const String cart = '/cart';
+  static const String order = '/order';
+  static const String orderDetail = '/orderDetail';
+  static const String address = '/address';
+  static const String addAddress = '/addAddress';
+  static const String editAddress = '/editAddress';
+  static const String payment = '/payment';
+  static const String checkout = '/checkout';
+  static const String notification = '/notification';
+  static const String search = '/search';
+  static const String contactUs = '/contactUs';
+  static const String aboutUs = '/aboutUs';
+  static const String faq = '/faq';
+  static const String privacyPolicy = '/privacyPolicy';
+  static const String termsAndConditions = '/termsAndConditions';
+   
+}
