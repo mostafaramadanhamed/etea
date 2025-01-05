212d421fa15ba5c123b6a5a5e60a72479e9f6ddc
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2025-01-05 10:22:49 +0200

    - added app colors

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryRed = Color(0xffF83758);
+  static const Color lightRed = Color(0xffFA7189);
+  static const Color lighterRed = Color(0xffFFCCD5);
+  static const Color darkBklue=Color(0xff17223B);
+  static const Color lightBlue=Color(0xff4392F9);
+  static const Color lightGrey=Color(0xffC4C4C4);
+
+  
+}
