3f5ac4c42d5084d624be867cc6d82c3b68bd3502
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2025-01-06 13:03:18 +0200

    - added slider model

---
+++ lib/features/onboarding/data/models/slider_model.dart
@@ -0,0 +1,9 @@
+class SliderModel{
+  final String image;
+  final String title;
+  final String description;
+
+  SliderModel({required this.image, required this.title, required this.description});
+
+
+}
