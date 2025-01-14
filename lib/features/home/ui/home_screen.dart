d3373c0121f82f80ad5f3eadbb6a7f74b627ca3f
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2025-01-14 09:51:11 +0200

    - added all featured

--- lib/features/home/ui/home_screen.dart
+++ lib/features/home/ui/home_screen.dart
@@ -1,9 +1,11 @@
 import 'package:etea/core/helper/spacing_extension.dart';
+import 'package:etea/core/theme/text_styles.dart';
 import 'package:etea/features/home/ui/widget/search_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'widget/home_app_bar.dart';
+import 'widget/text_icon_button.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -20,7 +22,26 @@ class HomeScreen extends StatelessWidget {
               16.ph,
               const HomeSearchBar(),
               16.ph,
-              
+              Row(
+                children: [
+                  Text(
+                    'All Featured',
+                    style: TextStyles.font18Bold,
+                  ),
+                  const Spacer(),
+                  TextIconButton(
+                    text: 'Sort',
+                    icon: Icons.swap_vert,
+                    onPressed: () {},
+                  ),
+                  8.pw,
+                  TextIconButton(
+                    text: 'Filter',
+                    icon: Icons.filter_alt_outlined,
+                    onPressed: () {},
+                  ),
+                ],
+              ),
             ],
           ),
         ),
