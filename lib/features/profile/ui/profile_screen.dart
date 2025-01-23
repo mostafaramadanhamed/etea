24a26a2fee77ed3c99a0d8c33648f26b66dbfcd6
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2025-01-23 10:09:23 +0200

    - added profile avatar

--- lib/features/profile/ui/profile_screen.dart
+++ lib/features/profile/ui/profile_screen.dart
@@ -1,10 +1,43 @@
+import 'package:etea/core/helper/spacing_extension.dart';
+import 'package:etea/core/theme/text_styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Profile'),
+        centerTitle: true,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0.w),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              30.ph,
+              Align(
+                alignment: AlignmentDirectional.center,
+                child: CircleAvatar(
+                  radius: 60.r,
+                  backgroundImage: AssetImage('assets/images/avatar.png'),
+                ),
+              ),
+              
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
