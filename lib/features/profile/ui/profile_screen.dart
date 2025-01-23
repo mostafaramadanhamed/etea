fde0827a46e186972e0db964a32498ecdd4ac1c9
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2025-01-23 10:13:00 +0200

    - added personal details

--- lib/features/profile/ui/profile_screen.dart
+++ lib/features/profile/ui/profile_screen.dart
@@ -1,7 +1,10 @@
 import 'package:etea/core/helper/spacing_extension.dart';
 import 'package:etea/core/theme/text_styles.dart';
+import 'package:etea/core/widgets/app_text_form_filed.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'widgets/title_with_text_filed.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
@@ -30,10 +33,22 @@ class ProfileScreen extends StatelessWidget {
                 alignment: AlignmentDirectional.center,
                 child: CircleAvatar(
                   radius: 60.r,
-                  backgroundImage: AssetImage('assets/images/avatar.png'),
+                  backgroundImage: const AssetImage('assets/images/avatar.png'),
                 ),
               ),
-              
+              28.ph,
+              Text(
+                'Personal Details',
+                style: TextStyles.font18Bold,
+              ),
+              20.ph,
+              const TitlewithTextFiled(
+                title: "Email Address",
+                hintText: "momomom@gmail.com",
+              ),
+              const TitlewithTextFiled(
+                  title: "Password", hintText: "********************"),
+              const Divider(),
             ],
           ),
         ),
